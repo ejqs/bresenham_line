937fb2953c76473e99ebf75cc9da1db4bd6856db
ejqs <62688314+ejqs@users.noreply.github.com>
2025-04-23 18:40:18 +0800

    initialize file structure

---
+++ bresenham_line.py
@@ -0,0 +1,14 @@
+# Processes Points and Generates
+
+def bresenham_line(point1 , point2):
+    """
+    Accepts point1 and point2 and returns a series of x,y 
+    coordinates for points to be laid out.
+    """
+    points = [] 
+    # Algorithm goes here
+
+
+    #
+    
+    return points # [(x, y), (x, y), ...]
